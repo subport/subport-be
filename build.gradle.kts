04af5e9274bcbb83020b635ee5f3d5f6a98d918f
yonghyeonpark <llyyoo93@gmail.com>
2026-01-04 23:20:22 +0900

    chore: 의존성 추가 및 수정
    - OCI 버킷 사용을 위한 AWS SDK 의존성 추가
    - 잘못된 JWT 의존성 수정
    - H2 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,14 +30,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("software.amazon.awssdk:s3:2.40.15")
 
     // JWT 관련
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
