cd099921be3cb1d781af250959520c9559c84e0e
yonghyeonpark <llyyoo93@gmail.com>
2026-01-09 20:20:35 +0900

    fix: 어댑터가 아닌 포트 주입으로 수정

--- src/main/java/subport/application/subscription/service/RegisterCustomSubscriptionService.java
+++ src/main/java/subport/application/subscription/service/RegisterCustomSubscriptionService.java
@@ -5,10 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
-import subport.adapter.out.objectstorage.OciObjectStorageAdapter;
 import subport.application.subscription.port.in.RegisterCustomSubscriptionRequest;
 import subport.application.subscription.port.in.RegisterCustomSubscriptionUseCase;
 import subport.application.subscription.port.out.SaveSubscriptionPort;
+import subport.application.subscription.port.out.UploadSubscriptionImagePort;
 import subport.domain.subscription.Subscription;
 import subport.domain.subscription.SubscriptionType;
 
@@ -17,7 +17,7 @@ import subport.domain.subscription.SubscriptionType;
 public class RegisterCustomSubscriptionService implements RegisterCustomSubscriptionUseCase {
 
 	private final SaveSubscriptionPort saveSubscriptionPort;
-	private final OciObjectStorageAdapter ociObjectStorageAdapter;
+	private final UploadSubscriptionImagePort uploadSubscriptionImagePort;
 
 	@Transactional
 	@Override
@@ -28,13 +28,15 @@ public class RegisterCustomSubscriptionService implements RegisterCustomSubscrip
 	) {
 		String logoImageUrl = null; // 기본 이미지 디자인 완성되면 대체
 		if (image != null) {
-			logoImageUrl = ociObjectStorageAdapter.upload(image);
+			logoImageUrl = uploadSubscriptionImagePort.upload(image);
 		}
 
 		Subscription subscription = Subscription.withoutId(
 			request.name(),
 			SubscriptionType.fromDisplayName(request.type()),
 			logoImageUrl,
+			null,
+			false,
 			memberId
 		);
 
