c607b8288ac56758b5efcd8bcc442cb10ef459d9
yonghyeonpark <llyyoo93@gmail.com>
2026-01-16 05:03:46 +0900

    refactor: 스케줄링 로직에 환율 정보 조회 반영

--- src/main/java/subport/application/spendingrecord/service/CreateSpendingRecordService.java
+++ src/main/java/subport/application/spendingrecord/service/CreateSpendingRecordService.java
@@ -1,5 +1,6 @@
 package subport.application.spendingrecord.service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import subport.application.membersubscription.port.out.LoadExchangeRatePort;
 import subport.application.membersubscription.port.out.LoadMemberSubscriptionPort;
 import subport.application.membersubscription.port.out.MemberSubscriptionDetail;
 import subport.application.membersubscription.port.out.UpdateMemberSubscriptionPort;
@@ -22,6 +24,7 @@ public class CreateSpendingRecordService implements CreateSpendingRecordUseCase 
 	private final LoadMemberSubscriptionPort loadMemberSubscriptionPort;
 	private final UpdateMemberSubscriptionPort updateMemberSubscriptionPort;
 	private final SaveSpendingRecordPort saveSpendingRecordPort;
+	private final LoadExchangeRatePort loadExchangeRatePort;
 
 	@Transactional
 	@Override
@@ -38,11 +41,21 @@ public class CreateSpendingRecordService implements CreateSpendingRecordUseCase 
 		List<MemberSubscription> memberSubscriptions = memberSubscriptionDetails.stream()
 			.map(detail -> {
 				MemberSubscription memberSubscription = detail.toMemberSubscription();
+
+				BigDecimal exchangeRate = memberSubscription.getExchangeRate();
+				LocalDate exchangeRateDate = memberSubscription.getExchangeRateDate();
+				if (exchangeRate != null && exchangeRateDate != null) {
+					LocalDate nextPaymentDate = memberSubscription.getNextPaymentDate();
+					exchangeRate = loadExchangeRatePort.load(nextPaymentDate.toString());
+					memberSubscription.updateExchangeRate(exchangeRate, nextPaymentDate);
+				}
+
 				memberSubscription.increaseNextPaymentDateByMonths(detail.planDurationMonths());
+
 				return memberSubscription;
 			})
 			.toList();
 
-		updateMemberSubscriptionPort.updateNextPaymentDate(memberSubscriptions);
+		updateMemberSubscriptionPort.update(memberSubscriptions);
 	}
 }
