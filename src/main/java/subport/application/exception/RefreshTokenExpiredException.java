275e851bd80c34b3319d67dc7cc575febc0779da
yonghyeonpark <llyyoo93@gmail.com>
2026-01-16 23:35:30 +0900

    refactor: 원본 예외를 받지 않는 생성자 추가

--- src/main/java/subport/application/exception/RefreshTokenExpiredException.java
+++ src/main/java/subport/application/exception/RefreshTokenExpiredException.java
@@ -2,6 +2,10 @@ package subport.application.exception;
 
 public class RefreshTokenExpiredException extends CustomException {
 
+	public RefreshTokenExpiredException() {
+		super(ErrorCode.REFRESH_TOKEN_EXPIRED);
+	}
+
 	public RefreshTokenExpiredException(Throwable cause) {
 		super(ErrorCode.REFRESH_TOKEN_EXPIRED, cause);
 	}
