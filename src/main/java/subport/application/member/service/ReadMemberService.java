a088c9ec08360589a524f82c5886ef21f642b7c9
yonghyeonpark <llyyoo93@gmail.com>
2026-01-23 03:56:25 +0900

    fix: 누락된 어노테이션 추가

--- src/main/java/subport/application/member/service/ReadMemberService.java
+++ src/main/java/subport/application/member/service/ReadMemberService.java
@@ -1,6 +1,7 @@
 package subport.application.member.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import subport.application.member.port.in.ReadMemberUseCase;
@@ -9,6 +10,7 @@ import subport.application.member.port.out.LoadMemberPort;
 import subport.domain.member.Member;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReadMemberService implements ReadMemberUseCase {
 
