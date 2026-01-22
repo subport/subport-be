48dd9e7c9965beb8f423c7657acd34c52e1a0e2b
yonghyeonpark <llyyoo93@gmail.com>
2026-01-23 02:33:39 +0900

    refactor: 환율 정보 조회 로직 분리 및 구독 정보 필드 변경 반영

--- src/main/java/subport/application/spendingrecord/service/CreateSpendingRecordService.java
+++ src/main/java/subport/application/spendingrecord/service/CreateSpendingRecordService.java
@@ -8,12 +8,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import subport.application.membersubscription.port.out.LoadExchangeRatePort;
+import subport.application.exchangeRate.service.ExchangeRateService;
 import subport.application.membersubscription.port.out.LoadMemberSubscriptionPort;
 import subport.application.membersubscription.port.out.UpdateMemberSubscriptionPort;
 import subport.application.membersubscription.port.out.dto.MemberSubscriptionForSpendingRecord;
 import subport.application.spendingrecord.port.in.CreateSpendingRecordUseCase;
 import subport.application.spendingrecord.port.out.SaveSpendingRecordPort;
+import subport.domain.exchangeRate.ExchangeRate;
 import subport.domain.membersubscription.MemberSubscription;
 import subport.domain.spendingrecord.SpendingRecord;
 
@@ -24,7 +25,7 @@ public class CreateSpendingRecordService implements CreateSpendingRecordUseCase 
 	private final LoadMemberSubscriptionPort loadMemberSubscriptionPort;
 	private final UpdateMemberSubscriptionPort updateMemberSubscriptionPort;
 	private final SaveSpendingRecordPort saveSpendingRecordPort;
-	private final LoadExchangeRatePort loadExchangeRatePort;
+	private final ExchangeRateService exchangeRateService;
 
 	@Transactional
 	@Override
@@ -42,14 +43,19 @@ public class CreateSpendingRecordService implements CreateSpendingRecordUseCase 
 			.map(detail -> {
 				MemberSubscription memberSubscription = detail.toMemberSubscription();
 
-				BigDecimal exchangeRate = memberSubscription.getExchangeRate();
+				BigDecimal rate = memberSubscription.getExchangeRate();
 				LocalDate exchangeRateDate = memberSubscription.getExchangeRateDate();
-				if (exchangeRate != null && exchangeRateDate != null) {
-					LocalDate nextPaymentDate = memberSubscription.getNextPaymentDate();
-					exchangeRate = loadExchangeRatePort.load(nextPaymentDate.toString());
-					memberSubscription.updateExchangeRate(exchangeRate, nextPaymentDate);
+				if (rate != null && exchangeRateDate != null) {
+					ExchangeRate exchangeRate =
+						exchangeRateService.getExchangeRate(memberSubscription.getNextPaymentDate());
+
+					rate = exchangeRate.getRate();
+					exchangeRateDate = exchangeRate.getApplyDate();
+
+					memberSubscription.updateExchangeRate(rate, exchangeRateDate);
 				}
 
+				memberSubscription.updateLastPaymentDate(detail.nextPaymentDate());
 				memberSubscription.increaseNextPaymentDateByMonths(detail.planDurationMonths());
 
 				return memberSubscription;
