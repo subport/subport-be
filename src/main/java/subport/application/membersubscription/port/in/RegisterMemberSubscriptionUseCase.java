923bf85175618b217f9f94aff54a31adbcf35699
yonghyeonpark <llyyoo93@gmail.com>
2026-01-23 03:38:00 +0900

    fix: 잘못 설정된 어노테이션 삭제

--- src/main/java/subport/application/membersubscription/port/in/RegisterMemberSubscriptionUseCase.java
+++ src/main/java/subport/application/membersubscription/port/in/RegisterMemberSubscriptionUseCase.java
@@ -1,11 +1,8 @@
 package subport.application.membersubscription.port.in;
 
-import org.springframework.stereotype.Service;
-
 import subport.application.membersubscription.port.in.dto.RegisterMemberSubscriptionRequest;
 import subport.application.membersubscription.port.in.dto.RegisterMemberSubscriptionResponse;
 
-@Service
 public interface RegisterMemberSubscriptionUseCase {
 
 	RegisterMemberSubscriptionResponse register(
