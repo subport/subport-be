754093d6a523484856dbb563b02467b59c11b458
yonghyeonpark <llyyoo93@gmail.com>
2026-01-23 03:59:49 +0900

    feat: 환율 정보 적용 날짜가 요청 날짜보다 하루전이라면 요청 날짜 기준으로 환율 정보를 다시 조회, 만약 데이터가 존재한다면 환율 적용 날짜를 업데이트 하도록 로직 추가

--- src/main/java/subport/application/exchangeRate/service/ExchangeRateService.java
+++ src/main/java/subport/application/exchangeRate/service/ExchangeRateService.java
@@ -2,13 +2,16 @@ package subport.application.exchangeRate.service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import subport.application.exchangeRate.port.out.FetchExchangeRatePort;
 import subport.application.exchangeRate.port.out.LoadExchangeRatePort;
 import subport.application.exchangeRate.port.out.SaveExchangeRatePort;
+import subport.application.exchangeRate.port.out.UpdateExchangeRatePort;
 import subport.domain.exchangeRate.ExchangeRate;
 
 @Service
@@ -18,12 +21,27 @@ public class ExchangeRateService {
 	private final LoadExchangeRatePort loadExchangeRatePort;
 	private final FetchExchangeRatePort fetchExchangeRatePort;
 	private final SaveExchangeRatePort saveExchangeRatePort;
+	private final UpdateExchangeRatePort updateExchangeRatePort;
 
+	@Transactional
 	public ExchangeRate getExchangeRate(LocalDate startDate) {
 		LocalDate startWeekdayDate = getLastWeekdayDate(startDate);
 
 		ExchangeRate exchangeRate = loadExchangeRatePort.load(startWeekdayDate);
 		if (exchangeRate != null) {
+			LocalDateTime noon = startWeekdayDate.atTime(12, 0);
+			LocalDateTime now = LocalDateTime.now();
+
+			if (exchangeRate.getApplyDate().equals(startWeekdayDate.minusDays(1))
+				&& exchangeRate.getLastModifiedAt().isBefore(noon)
+				&& !now.isBefore(noon)) {
+				BigDecimal rate = fetchExchangeRatePort.fetch(startWeekdayDate.toString());
+				if (rate != null) {
+					exchangeRate.updateRate(startWeekdayDate, rate);
+					updateExchangeRatePort.update(exchangeRate);
+				}
+			}
+
 			return exchangeRate;
 		}
 
@@ -36,7 +54,12 @@ public class ExchangeRateService {
 			}
 		}
 
-		exchangeRate = new ExchangeRate(startWeekdayDate, targetDate, rate);
+		exchangeRate = new ExchangeRate(
+			startWeekdayDate,
+			targetDate,
+			rate,
+			null
+		);
 		saveExchangeRatePort.save(exchangeRate);
 
 		return exchangeRate;
