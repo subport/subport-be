14163b7037f478e7644ea5f2b843abcf3286d214
yonghyeonpark <llyyoo93@gmail.com>
2026-01-26 02:53:59 +0900

    chore: 메일 발송 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("software.amazon.awssdk:s3:2.40.15")
 
     // JWT 관련
