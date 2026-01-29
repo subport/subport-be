ed0c085eb0a79fb08ad33b53463b58df72274789
yonghyeonpark <llyyoo93@gmail.com>
2026-01-29 14:37:06 +0900

    feat: 구독 서비스 단건 조회 DTO에 logoImageUrl 추가

--- src/main/java/subport/application/subscription/port/in/dto/GetSubscriptionResponse.java
+++ src/main/java/subport/application/subscription/port/in/dto/GetSubscriptionResponse.java
@@ -5,14 +5,16 @@ import subport.domain.subscription.Subscription;
 public record GetSubscriptionResponse(
 	Long id,
 	String name,
-	String type
+	String type,
+	String logoImageUrl
 ) {
 
 	public static GetSubscriptionResponse from(Subscription subscription) {
 		return new GetSubscriptionResponse(
 			subscription.getId(),
 			subscription.getName(),
-			subscription.getType().getDisplayName()
+			subscription.getType().getDisplayName(),
+			subscription.getLogoImageUrl()
 		);
 	}
 }
