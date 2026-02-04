b8facb95e0200d5d9bf958b6b51231fe1d0f4acb
yonghyeonpark <llyyoo93@gmail.com>
2026-02-05 04:38:36 +0900

    fix: 생성하는 구독 정보의 다음 결제일이 오늘 날짜보다 이전이라면 소비 내역을 생성하도록 수정

--- src/main/java/subport/application/membersubscription/service/RegisterMemberSubscriptionService.java
+++ src/main/java/subport/application/membersubscription/service/RegisterMemberSubscriptionService.java
@@ -3,6 +3,8 @@ package subport.application.membersubscription.service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +18,14 @@ import subport.application.membersubscription.port.in.RegisterMemberSubscription
 import subport.application.membersubscription.port.in.dto.RegisterMemberSubscriptionRequest;
 import subport.application.membersubscription.port.in.dto.RegisterMemberSubscriptionResponse;
 import subport.application.membersubscription.port.out.SaveMemberSubscriptionPort;
+import subport.application.spendingrecord.port.out.LoadSpendingRecordPort;
+import subport.application.spendingrecord.port.out.SaveSpendingRecordPort;
 import subport.application.subscription.port.out.LoadPlanPort;
 import subport.application.subscription.port.out.LoadSubscriptionPort;
 import subport.domain.exchangeRate.ExchangeRate;
 import subport.domain.member.Member;
 import subport.domain.membersubscription.MemberSubscription;
-import subport.domain.subscription.AmountUnit;
+import subport.domain.spendingrecord.SpendingRecord;
 import subport.domain.subscription.Plan;
 import subport.domain.subscription.Subscription;
 
@@ -35,6 +39,8 @@ public class RegisterMemberSubscriptionService implements RegisterMemberSubscrip
 	private final LoadPlanPort loadPlanPort;
 	private final ExchangeRateService exchangeRateService;
 	private final LoadMemberPort loadMemberPort;
+	private final LoadSpendingRecordPort loadSpendingRecordPort;
+	private final SaveSpendingRecordPort saveSpendingRecordPort;
 
 	@Override
 	public RegisterMemberSubscriptionResponse register(
@@ -42,6 +48,9 @@ public class RegisterMemberSubscriptionService implements RegisterMemberSubscrip
 		RegisterMemberSubscriptionRequest request,
 		LocalDateTime currentDateTime
 	) {
+		LocalDate startDate = request.startDate();
+		validateStartDate(startDate, currentDateTime.toLocalDate());
+
 		boolean dutchPay = request.dutchPay();
 		BigDecimal dutchPayAmount = request.dutchPayAmount();
 		if (dutchPay && dutchPayAmount == null) {
@@ -67,8 +76,7 @@ public class RegisterMemberSubscriptionService implements RegisterMemberSubscrip
 
 		BigDecimal rate = null;
 		LocalDate exchangeRateDate = null;
-		LocalDate startDate = request.startDate();
-		if (plan.getAmountUnit().name().equals(AmountUnit.USD.name())) {
+		if (plan.isUsdBased()) {
 			ExchangeRate exchangeRate = exchangeRateService.getExchangeRate(startDate, currentDateTime);
 
 			rate = exchangeRate.getRate();
@@ -93,6 +101,73 @@ public class RegisterMemberSubscriptionService implements RegisterMemberSubscrip
 			plan
 		);
 
-		return new RegisterMemberSubscriptionResponse(saveMemberSubscriptionPort.save(memberSubscription));
+		MemberSubscription savedMemberSubscription = saveMemberSubscriptionPort.save(memberSubscription);
+
+		List<SpendingRecord> spendingRecords = createSpendingRecords(savedMemberSubscription, currentDateTime);
+		saveSpendingRecordPort.save(spendingRecords);
+
+		return new RegisterMemberSubscriptionResponse(savedMemberSubscription.getId());
+	}
+
+	private void validateStartDate(LocalDate startDate, LocalDate currentDate) {
+		if (startDate.isAfter(currentDate)) {
+			throw new CustomException(ErrorCode.INVALID_START_DATE_FUTURE);
+		}
+		if (startDate.isBefore(currentDate.minusYears(1))) {
+			throw new CustomException(ErrorCode.INVALID_START_DATE_TOO_OLD);
+		}
+	}
+
+	private List<SpendingRecord> createSpendingRecords(
+		MemberSubscription memberSubscription,
+		LocalDateTime currentDateTime
+	) {
+		List<SpendingRecord> spendingRecords = new ArrayList<>();
+		LocalDate nextPaymentDate = memberSubscription.getNextPaymentDate();
+		while (!nextPaymentDate.isAfter(currentDateTime.toLocalDate())) {
+			SpendingRecord spendingRecord;
+			if (nextPaymentDate.isEqual(currentDateTime.toLocalDate())
+				&& !loadSpendingRecordPort.existsSpendingRecord(nextPaymentDate, memberSubscription.getId())) {
+				spendingRecord = createSpendingRecord(memberSubscription, currentDateTime);
+				spendingRecords.add(spendingRecord);
+				break;
+			}
+
+			spendingRecord = createSpendingRecord(memberSubscription, currentDateTime);
+			spendingRecords.add(spendingRecord);
+
+			nextPaymentDate = memberSubscription.getNextPaymentDate();
+		}
+
+		return spendingRecords;
+	}
+
+	private SpendingRecord createSpendingRecord(MemberSubscription memberSubscription, LocalDateTime currentDateTime) {
+		Subscription subscription = memberSubscription.getSubscription();
+		SpendingRecord spendingRecord = new SpendingRecord(
+			memberSubscription.getLastPaymentDate(),
+			memberSubscription.calculateActualPaymentAmount(),
+			memberSubscription.getPlan().getDurationMonths(),
+			subscription.getName(),
+			subscription.getLogoImageUrl(),
+			memberSubscription.getMember().getId(),
+			memberSubscription.getId()
+		);
+
+		memberSubscription.updateLastPaymentDate();
+
+		if (memberSubscription.getExchangeRate() != null) {
+			ExchangeRate exchangeRate = exchangeRateService.getExchangeRate(
+				memberSubscription.getLastPaymentDate(),
+				currentDateTime
+			);
+
+			memberSubscription.updateExchangeRate(
+				exchangeRate.getRate(),
+				exchangeRate.getApplyDate()
+			);
+		}
+
+		return spendingRecord;
 	}
 }
