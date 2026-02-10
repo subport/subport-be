39021b1274fca5ecde94b52b68b086ad207f5e31
yonghyeonpark <llyyoo93@gmail.com>
2026-02-11 05:56:26 +0900

    feat: 어드민용 구독 서비스 목록 조회 응답 DTO에 페이지네이션 관련 정보 추가

--- src/main/java/subport/admin/application/dto/AdminSubscriptionsResponse.java
+++ src/main/java/subport/admin/application/dto/AdminSubscriptionsResponse.java
@@ -3,6 +3,9 @@ package subport.admin.application.dto;
 import java.util.List;
 
 public record AdminSubscriptionsResponse(
-	List<AdminSubscriptionResponse> subscriptions
+	List<AdminSubscriptionResponse> subscriptions,
+	int currentPage,
+	long totalElements,
+	int totalPages
 ) {
 }
