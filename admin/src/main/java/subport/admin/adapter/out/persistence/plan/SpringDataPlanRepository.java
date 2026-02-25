7acb8cdce877ddc2e1334107d03b94c7a666dc6d
yonghyeonpark <llyyoo93@gmail.com>
2026-02-25 21:13:13 +0900

    fix(admin): 시스템 기본 제공 플랜만 조회하도록 수정

--- admin/src/main/java/subport/admin/adapter/out/persistence/plan/SpringDataPlanRepository.java
+++ admin/src/main/java/subport/admin/adapter/out/persistence/plan/SpringDataPlanRepository.java
@@ -3,10 +3,17 @@ package subport.admin.adapter.out.persistence.plan;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import subport.domain.plan.Plan;
 
 public interface SpringDataPlanRepository extends JpaRepository<Plan, Long> {
 
+	@Query("""
+		SELECT p
+		FROM Plan p
+		WHERE p.subscription.id = :subscriptionId
+		AND p.systemProvided = true
+		""")
 	List<Plan> findBySubscriptionId(Long subscriptionId);
 }
