5ac9d6de9797544f5053f8c963c5fa77d80bb25f
yonghyeonpark <llyyoo93@gmail.com>
2026-02-27 21:20:44 +0900

    chore(api, batch, admin, domain): @EnableJpaAuditing을 domain 모듈에 적용하여 공통화

---
+++ domain/src/main/java/subport/domain/JpaConfig.java
@@ -0,0 +1,9 @@
+package subport.domain;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
